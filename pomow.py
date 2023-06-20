215c50e3a89928e794bda89fe93d2393d3455582
linxuil <linxuil.g@gmail.com>
2023-06-21 02:22:40 +0300

    feat: Simple GUI interface for the Pomodoro timer

--- pomow.py
+++ pomow.py
@@ -3,19 +3,57 @@
 """pomow.py: Implementation a Pomodoro timer"""
 __author__ = "linxuil"
 
-import re
-from typing import List, Pattern, Union
-from io import TextIOWrapper
-from src.convert_to_list import convert_to_list
-from src.search_in_list import search_in_list
+import tkinter as tk
 
-def pomow(pattern: str, *args: Union[List[str], str, TextIOWrapper]) -> List[str]:
-    if not pattern:
-        raise ValueError("Pattern must be a non-empty string")
-    
-    if not args:
-        raise ValueError("At least one variable must be provided")
-    
-    # <!...!>
-    
-    return result
+class TimerApp:
+    def __init__(self, master):
+        self.master = master
+        self.time_left = 25 * 60
+        self.is_paused = True
+        self.master.title("Pomodoro Timer")
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.canvas = tk.Canvas(self.master, width=400, height=400, bg='white')
+        self.canvas.pack()
+        self.draw_timer()
+        self.start_button = tk.Button(self.master, text="Start", command=self.start_timer)
+        self.start_button.pack(side=tk.LEFT)
+        self.pause_button = tk.Button(self.master, text="Pause", command=self.pause_timer)
+        self.pause_button.pack(side=tk.LEFT)
+        self.reset_button = tk.Button(self.master, text="Reset", command=self.reset_timer)
+        self.reset_button.pack(side=tk.LEFT)
+
+    def draw_timer(self):
+        self.canvas.delete("all")
+        x, y, r = 200, 200, 150  # center coordinates and radius
+        self.canvas.create_oval(x-r, y-r, x+r, y+r)  # draw circle
+        time_text = str(self.time_left // 60) + ":" + str(self.time_left % 60).zfill(2)
+        self.canvas.create_text(x, y, text=time_text, font=("Arial", 24))
+
+    def start_timer(self):
+        if self.is_paused:
+            self.is_paused = False
+            self.update_timer()
+
+    def pause_timer(self):
+        if not self.is_paused:
+            self.is_paused = True
+
+    def reset_timer(self):
+        self.time_left = 25 * 60
+        self.draw_timer()
+
+    def update_timer(self):
+        if self.is_paused:
+            return
+        self.time_left -= 1
+        if self.time_left < 0:
+            self.time_left = 25 * 60
+        self.draw_timer()
+        self.master.after(1000, self.update_timer)
+
+
+root = tk.Tk()
+app = TimerApp(root)
+root.mainloop()
